f6558cc9ecfcfe034a5c9ff767622d8369157115
unknown <nmckenzie@zappos.com>
2016-06-07 23:28:19 -0700

    add a little js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 //alert("This is an alert I created in index.js!");
+$(document).ready(function() {
+/*	$('anlcost').on('click', function() {
+		var amount = $(this).closest('.habit').('.wklycost');
+		var calc = $(' $'+amount*52);
+	$(this).addClass('calc');
+	$(this).remove();*/
+	$(".btn".on('click',function() {
+		console.log('Read on!');
+		alert("so tired.");
+});
+});
