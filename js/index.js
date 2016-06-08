a848aea794b957ab2a1a232998d80f4f119821ce
unknown <nmckenzie@zappos.com>
2016-06-08 12:35:56 -0700

    add a webform

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
 //alert("This is an alert I created in index.js!");
-$(document).ready(function() {
-/*	$('anlcost').on('click', function() {
-		var amount = $(this).closest('.habit').('.wklycost');
-		var calc = $(' $'+amount*52);
-	$(this).addClass('calc');
-	$(this).remove();*/
-	$(".btn".on('click',function() {
-		console.log('Read on!');
-		alert("so tired.");
-});
+$( document ).ready(function() {
+    var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
 });
